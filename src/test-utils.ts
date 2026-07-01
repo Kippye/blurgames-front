d69a7a62f42a43c53cd3dde7f407231944c40892
KipHub <remo.auvaart@gmail.com>
2026-07-01 20:54:35 +0300

    Remove unused files

--- src/test-utils.ts
+++
@@ -1,9 +0,0 @@
-import { VueWrapper } from '@vue/test-utils';
-
-/**
- * Type assertion helper for wrapper.vm to avoid TypeScript errors
- * Usage: const vm = getWrapperVm<SalonDetailViewInstance>(wrapper);
- */
-export function getWrapperVm<T>(wrapper: VueWrapper): T {
-  return wrapper.vm as unknown as T;
-}
